b8d6592df9a8d07d0f90c5e3495eeb7a55fcf2db
steve chesnowitz <steve@chesnowitz.com>
2016-02-22 06:17:36 +0000

    working on design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,7 @@ class PagesController < ApplicationController
   before_filter :allow_iframe_requests
   def index
   end
+  
+  def projects
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   
   resources :quotes
   devise_for :users, :controllers => { registrations: 'registrations' }
-  get 'pages/index'
+  get 'pages/projects'
+  get 'pages/index' 
   root to: 'creatives#index'
  
 end
