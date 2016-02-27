a6f74907e67bca090c8d16bae94d91f9afa5243b
steve chesnowitz <steve@chesnowitz.com>
2016-02-27 02:32:51 +0000

    test contact form in production

--- app/controllers/creatives_controller.rb
+++ app/controllers/creatives_controller.rb
@@ -3,6 +3,7 @@ class CreativesController < ApplicationController
   before_filter :allow_iframe_requests
 
   def index
+    @contact = Contact.new
     if current_user
       redirect_to pages_index_path 
     end
--- gp
+++ gp
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "reset email addresss.chesnowitz@gmail.com" 
+git commit -m "test contact form in production" 
 git push origin master
 git push heroku master
