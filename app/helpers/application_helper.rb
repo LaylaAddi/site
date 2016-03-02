81a1cb6fcd0f3e81f8fe3345e8e262110064c330
steve chesnowitz <steve@chesnowitz.com>
2016-03-02 14:53:56 +0000

    working on the messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,20 @@
 module ApplicationHelper
   
+  def bootstrap_class_for flash_type
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-error"
+      when :alert
+        "alert-block"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
+  
   def page_header(text)
     content_for(:page_header) { text.to_s }
   end
